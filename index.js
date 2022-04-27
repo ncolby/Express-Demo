9bea8fe5c0c07d663e518045807ce53f4db2e22c
ncolby <nikko.j.colby@gmail.com>
2022-04-27 16:19:03 -0400

    for push to heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 server.listen(PORT, () => {
   console.log("Server Listening.......");
 });
-
+// checking for push to heroku
 server.get("/nikko", (req, res) => {
   res.send("<h1>Hi Nikko! </h1>");
 });
