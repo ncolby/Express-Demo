cdad30db4f092eb41cbc04cd10f6912ffed2cd14
ncolby <nikko.j.colby@gmail.com>
2022-04-27 16:57:42 -0400

    updated process.env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const server = express();
 // adding stuff to make sure issue is fixed
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 // not working?
 server.listen(PORT, () => {
   console.log("Server Listening.......");
